c72d905aba3689054852393b02a6e01c7c0a904d
pranoybiswas1994 <pranoybiswas1994@gmail.com>
2018-07-21 08:48:24 -0400

    content change

--- faq.php
+++ faq.php
@@ -31,50 +31,70 @@
                 <li style="margin-top: 8%">
                     <p style="font-weight:bold">What is CloudRest?</p>
                     <p style="margin-top: -2%">
-                        CloudRest is a health-tech startup with a vision of delivering health & wealth to everybody. We deliver health through our products & wealth through our investment plans.
+                       CloudRest is a health-tech startup with a vision of delivering health &amp; wealth to everybody. We deliver
+health through our products &amp; wealth through our
+investment plans.
                     </p>
                 </li>
 
                 <li style="margin-top: 5%">
-                    <p style="font-weight:bold">How long does it take to get my return on investment?</p>
+                    <p style="font-weight:bold">How long does it take to start getting my returns?</p>
                     <p style="margin-top: -2%">
-                       Within 12 months. Period. To know more about how this Returns plan works visit our INVESTMENT PLAN page. 
+                       You will start getting returns from following month of
+your investment. That is 30 days from the date we
+receive your investment and signing the lease back
+agreement. Rent will be transferred to your account
+directly on or before 10 th of every month. 
                     </p>
                 </li>
 
                 <li style="margin-top: 5%">
                     <p style="font-weight:bold">Who uses the assets purchased from your investment?</p>
                     <p style="margin-top: -2%">
-                        Your assets are deployed in places with heavy footfalls such as Malls, Hypermarkets, Co-Working Office Spaces, IT Parks, Coporate Offices, Salons, Gyms, Fitness Centers, & not to forget our portable massagers in Cabs & Buses, etc,.
-                    </p>
+                        Your assets are deployed in places with heavy footfalls
+such as Malls, Airports. Hypermarkets, Co-Working
+Office Spaces, IT Parks, Corporate Offices, Salons,
+Gyms, Fitness Centers, &amp; not to forget our portable
+massagers in Cabs &amp; Buses, etc,.</p>
                 </li>
 
                 <li style="margin-top: 5%">
                     <p style="font-weight:bold">What is the expected income?</p>
                     <p style="margin-top: -2%">
-                        You can always expect a return of around 5% to 8% every month. This can vary depending on the Return Scheme you opt for. Read more about the Returns Scheme here.
-                    </p>
+                        You get Monthly Rent @ 4% on the value of the
+Machine that you have invested ,which translates to
+48% ROI per year.</p>
                 </li>
 
                 <li style="margin-top: 5%">
                     <p style="font-weight:bold">How this is a zero-risk investment?</p>
                     <p style="margin-top: -2%">
-                        You’re investments are 100% secure & safe. CloudRest does NOT use your money in secret transactions. We invest your money immediately on physical & real healthcare products that come with unique MAC ID that can be tracked through our app. You will also get Machine Ownership Document, invoice & insurance in your name. So you know when & where you money is invested — with complete proof.
-                    </p>
+                        You’re investments are 100% secure &amp; safe as it is on
+tangible products. We invest your money immediately
+on physical &amp; real healthcare products that come with
+unique MAC ID that can be tracked through our app.
+You will also get Machine Ownership
+Document/Invoice in your name. So you know when &amp;
+where you money is invested — with complete proof.</p>
                 </li>
 
                 <li style="margin-top: 5%">
                     <p style="font-weight:bold">Does your company has RBI Approval ?</p>
                     <p style="margin-top: -2%">
-                        Since you have invested your money on our products & we lease it back from you — we don’t technically come under RBI regulations.
-                    </p>
+                        Since you are investing your money on our products &amp;
+we lease it back from you — we don’t technically come
+under RBI regulations.</p>
                 </li>
 
                 <li style="margin-top: 5%">
-                    <p style="font-weight:bold">I’m interested but I need more information before I take a decision.</p>
+                    <p style="font-weight:bold">I’m interested but I need more information before I
+take a decision.</p>
                     <p style="margin-top: -2%">
-                        Of course. We wouldn’t want you to take a decision immediately if you’re not sure. Talk to use & we will help you with all your queries. Fill this form below & someone from our team will call you or set up a meeting. 
-                    </p>
+                        Of course. We wouldn’t want you to take a decision
+immediately if you’re not sure. Talk to use &amp; we will
+help you with all your queries. Fill this form below &amp;
+someone from our team will call you or set up a
+meeting.</p>
                 </li>
             </ul>
         </div>
